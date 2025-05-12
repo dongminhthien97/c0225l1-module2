7011d240e101bfa2c34288a12357a6edd228270b
Minhthien <dongminhthiendn97@gmail.com>
2025-05-12 19:38:47 +0700

    hien thi loi chao

---
+++ ss1_tong_quan_java/bai_tap/Helloworld/src/hien_thi_loi_chao/hien_thi_loi_chao.java
@@ -0,0 +1,11 @@
+package hien_thi_loi_chao;
+import  java.util.Scanner;
+
+public class hien_thi_loi_chao {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter name : ");
+        String name = sc.nextLine();
+        System.out.println(" Hello: " + name);
+    }
+}
