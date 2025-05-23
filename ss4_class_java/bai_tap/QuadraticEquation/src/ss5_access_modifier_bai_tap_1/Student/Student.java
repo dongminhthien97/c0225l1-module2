4608b6b7f76e9354e38024d25af2a6731fa49230
Minhthien <dongminhthiendn97@gmail.com>
2025-05-23 21:10:18 +0700

    Student

---
+++ ss4_class_java/bai_tap/QuadraticEquation/src/ss5_access_modifier_bai_tap_1/Student/Student.java
@@ -0,0 +1,25 @@
+package ss5_access_modifier_bai_tap_1.Student;
+
+public class Student {
+    private String name = "John";
+    private String classes = "C02";
+
+    public Student() {
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setClasses(String Class) {
+        this.classes = classes;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", classes='" + classes + '\'' +
+                '}';
+    }
+}
---
+++ ss4_class_java/bai_tap/QuadraticEquation/src/ss5_access_modifier_bai_tap_1/Student/StudentTest.java
@@ -0,0 +1,10 @@
+package ss5_access_modifier_bai_tap_1.Student;
+
+public class StudentTest {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setName("Thien");
+        student.setClasses("C0002");
+        System.out.println(student.toString());
+    }
+}
