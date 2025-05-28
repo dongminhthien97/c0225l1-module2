35831dcfc2829400c6390d2ede2ca327c8d8c648
Minhthien <dongminhthiendn97@gmail.com>
2025-05-28 19:22:08 +0700

    Point2DPoint3D

---
+++ ss4_class_java/bai_tap/QuadraticEquation/src/ss6_ke_thua/bai_tap/bai_tap_2/PointTest.java
@@ -0,0 +1,12 @@
+package ss6_ke_thua.bai_tap.bai_tap_2;
+
+import java.awt.*;
+
+public class PointTest {
+    public static void main(String[] args) {
+        Point2D point2D = new Point2D(5, 5);
+        System.out.println(point2D);
+        Point3D point3D = new Point3D(5, 5, 5);
+        System.out.println(point3D);
+    }
+}
